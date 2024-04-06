180ae393d001e24a5e7e22adc704114a22ede9da
Charles Munat <charles.munat@gmail.com>
2024-04-06 17:21:16 +1300

    chore: update tests

--- lib/array/map/index.test.ts
+++ lib/array/map/index.test.ts
@@ -5,12 +5,12 @@ import map from "."
 const arr = [1, 2, 3, 4, 5]
 
 test("returns a new mapped array", () => {
-	expect(map<number, number>(n => n * n)(arr)).toStrictEqual([1, 4, 9, 16, 25])
+	expect(map(n => n * n)(arr)).toStrictEqual([1, 4, 9, 16, 25])
 })
 
 test("returns an empty array when the array is empty", () => {
 	const empty = [] as Array<number>
 
-	expect(map<number, number>(n => n * n)([])).toStrictEqual(empty)
+	expect(map(n => n * n)([])).toStrictEqual(empty)
 	expect(map(n => n)([]) === empty).toBeFalsy() // new array
 })
--- lib/array/reduce/index.test.ts
+++ lib/array/reduce/index.test.ts
@@ -5,17 +5,13 @@ import reduce from "."
 const arr = [1, 2, 3, 4, 5]
 
 test("loops through the array producing a new output based on the initial value", () => {
-	expect(reduce<number, number>((sum, n) => sum + n)(0)(arr)).toBe(15)
-	expect(reduce<number, number>((sum, n) => sum + n)(10)(arr)).toBe(25)
-	expect(
-		reduce<number, number[]>((acc, n) => [...acc, n])([] as Array<number>)(arr),
-	).toStrictEqual(arr)
+	expect(reduce((sum, n) => sum + n)(0)(arr)).toBe(15)
+	expect(reduce((sum, n) => sum + n)(10)(arr)).toBe(25)
+	expect(reduce((acc, n) => [...acc, n])([])(arr)).toStrictEqual(arr)
 })
 
 test("returns the initial value when the array is empty", () => {
-	expect(reduce<number, number>((sum, n) => sum + n)(0)([])).toBe(0)
-	expect(reduce<number, number>((sum, n) => sum + n)(10)([])).toBe(10)
-	expect(
-		reduce<number, number[]>((acc, n) => [...acc, n])([] as Array<number>)([]),
-	).toStrictEqual([])
+	expect(reduce((sum, n) => sum + n)(0)([])).toBe(0)
+	expect(reduce((sum, n) => sum + n)(10)([])).toBe(10)
+	expect(reduce((acc, n) => [...acc, n])([])([])).toStrictEqual([])
 })
