92e32d3d883e15f2aa2bc05cf9fb15b695dcf42f
Charles Munat <charles.munat@gmail.com>
2024-04-12 19:26:42 +1200

    chore: merge dep changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import dts from "vite-plugin-dts"
 import { defineConfig } from "vite"
 import { resolve } from "path"
 
 export default defineConfig({
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "index.ts"),
